c5a41aa0e760217d22d21dccae9a14b918349533
ankitbanshi <banshiwalankit448@gmail.com>
2026-02-07 12:26:26 +0530

    Added Form for user input

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
-import { Home } from './pages/Home.tsx'
+import { Home} from'./pages/Home.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
-
+import Form from './pages/Form.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path='/' element={<App />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/Form' element={<Form/>}/>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
